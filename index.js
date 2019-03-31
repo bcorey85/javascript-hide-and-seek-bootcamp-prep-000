73a92ff76ee8f9016a0da36df734de7de1f57f60
brandon corey <bcorey85@gmail.com>
2019-03-31 15:00:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function getFirstSelector(selector){
-  return(document.querySelector(selector))
+  return document.querySelector(selector)
 }
-
 
 function nestedTarget() {
 return(document.getElementById('nested').querySelector('div.target'))
@@ -11,31 +10,12 @@ return(document.getElementById('nested').querySelector('div.target'))
 function increaseRankBy(n) {
   var liValue = document.querySelectorAll('.ranked-list li');
   for (var i = 0; i < liValue.length; i++) {
-   var integer = parseInt(liValue[i].innerHTML) + n
-   liValue[i].innerHTML = integer
+  var integer = parseInt(liValue[i].innerHTML) + n
+  liValue[i].innerHTML = integer
   }
 }
 
-const divValue = document.getElementById('grand-node').querySelectorAll('div')
-
-
-
-function deepestChild() {
-    var current = document.getElementById('grand-node').querySelectorAll('div')
-    return current[current.length - 1]
-  }
-
-//  var next = []
-// while (current) {
-// 		for (let i = 0; i < current.length; i++) {
-//       if(current[i].innerHTML === "boo!"){
-//       return current[i].innerHTML
-//     }else{
-//     console.log(current[i])
-// 		next.push(current[i])
-// 	}
-// 	}
-// 	current = next.shift()
-// 	}
-// 	return null
-// }
+function deepestChild(){
+  var node = document.querySelectorAll('#grand-node div')
+  return node[node.length - 1]
+}
